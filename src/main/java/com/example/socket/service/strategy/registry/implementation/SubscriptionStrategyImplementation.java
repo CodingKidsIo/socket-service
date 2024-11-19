b3e224f9dd32842588bc9ab3b57171a80af098d3
Coding Kids <codingkids.internal@gmail.com>
2024-11-19 12:35:53 +0530

    graceful exception handling

--- src/main/java/com/example/socket/service/strategy/registry/implementation/SubscriptionStrategyImplementation.java
+++ src/main/java/com/example/socket/service/strategy/registry/implementation/SubscriptionStrategyImplementation.java
@@ -6,12 +6,17 @@ import java.util.Map;
 import org.springframework.stereotype.Service;
 
 import com.example.socket.service.enumeration.SubscriptionType;
+import com.example.socket.service.request.BaseRequest;
 import com.example.socket.service.strategy.registry.SubscriptionStrategy;
 import com.example.socket.service.strategy.service.SubscriptionService;
 import com.example.socket.service.strategy.service.implementation.PriceFeedSubscriptionService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class SubscriptionStrategyImplementation implements SubscriptionStrategy {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     private final Map<SubscriptionType, SubscriptionService> subscriptionServices;
 
     public SubscriptionStrategyImplementation() {
@@ -20,14 +25,25 @@ public class SubscriptionStrategyImplementation implements SubscriptionStrategy 
     }
 
     @Override
-    public void subscribe(SubscriptionType subscriptionType, String sessionId, Object data) {
-        SubscriptionService service = subscriptionServices.get(subscriptionType);
-        service.subscribe(sessionId, data);
+    public void subscribe(String sessionId, String data) {
+        BaseRequest<Object> baseRequest = getBaseRequest(data);
+        SubscriptionService service = subscriptionServices.get(baseRequest.getType());
+        service.subscribe(sessionId, baseRequest.getData());
     }
 
     @Override
-    public void unsubscribe(SubscriptionType type, String sessionId, Object data) {
-        SubscriptionService service = subscriptionServices.get(type);
-        service.unsubscribe(sessionId, data);
+    public void unsubscribe(String sessionId, String data) {
+        BaseRequest<Object> baseRequest = getBaseRequest(data);
+        SubscriptionService service = subscriptionServices.get(baseRequest.getType());
+        service.unsubscribe(sessionId, baseRequest.getData());
     }
+
+    private BaseRequest<Object> getBaseRequest(String data) {
+        try {
+            return objectMapper.readValue(data, new TypeReference<BaseRequest<Object>>() {});
+        } catch (JsonProcessingException exception) {
+            throw new IllegalArgumentException("Failed to parse data", exception);
+        }
+    }
+
 }
